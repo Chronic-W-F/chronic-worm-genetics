4826869de45864287d3c10cbc104e8c5bddcace9
Chronic-W-F <vandergriff4004@comcast.net>
2026-05-10 01:32:31 -0500

    Update strains.js

--- data/strains.js
+++ data/strains.js
@@ -1,1 +1,103 @@
+const strains = [
 
+{
+  code: "CW-01",
+  name: "Cherry Bomb",
+  type: "Photo Reg",
+  pack: "8-Pack",
+  thc: "17-22%",
+  description: "Sativa Leaning • Fruity • Balanced",
+  image: "cherry-bomb.png"
+},
+
+{
+  code: "CW-02",
+  name: "Blueberry Kush",
+  type: "Photo Reg",
+  pack: "8-Pack",
+  thc: "High THC",
+  description: "Indica Dominant • Sweet Berry • Relaxing",
+  image: "blueberry-kush.png"
+},
+
+{
+  code: "CW-03",
+  name: "LSD",
+  type: "Photo Reg",
+  pack: "8-Pack",
+  thc: "24%",
+  description: "Hybrid • Creative • Uplifting",
+  image: "lsd.png"
+},
+
+{
+  code: "CW-04",
+  name: "Death Bubba",
+  type: "Photo Reg",
+  pack: "8-Pack",
+  thc: "High THC",
+  description: "Indica Dominant • Heavy • Sedative",
+  image: "death-bubba.png"
+},
+
+{
+  code: "CW-05",
+  name: "Purple Rock Candy",
+  type: "Photo Reg",
+  pack: "8-Pack",
+  thc: "High THC",
+  description: "Indica • Sweet • Relaxing",
+  image: "purple-rock-candy.png"
+},
+
+{
+  code: "CW-06",
+  name: "Spiked Punch",
+  type: "Photo Reg",
+  pack: "8-Pack",
+  thc: "24%",
+  description: "Hybrid • Balanced • Strong",
+  image: "spiked-punch.png"
+},
+
+{
+  code: "CW-07",
+  name: "Red Dragon",
+  type: "Photo Reg",
+  pack: "8-Pack",
+  thc: "22%",
+  description: "Sativa Hybrid • Energetic",
+  image: "red-dragon.png"
+},
+
+{
+  code: "CW-08",
+  name: "Red Dawn",
+  type: "Photo Reg",
+  pack: "8-Pack",
+  thc: "High THC",
+  description: "Hybrid • Balanced",
+  image: "red-dawn.png"
+},
+
+{
+  code: "CW-09",
+  name: "Purple Dawn",
+  type: "Photo Reg",
+  pack: "8-Pack",
+  thc: "24-26%",
+  description: "Indica Dominant • Relaxing",
+  image: "purple-dawn.png"
+},
+
+{
+  code: "CW-10",
+  name: "Purple Punch",
+  type: "Photo Reg",
+  pack: "8-Pack",
+  thc: "High THC",
+  description: "Indica Leaning • Sweet",
+  image: "purple-punch.png"
+}
+
+];

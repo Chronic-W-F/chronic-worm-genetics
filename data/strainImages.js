7ae192f9a50c4bcb61131e58a761b1cfe69a534b
Chronic-W-F <vandergriff4004@comcast.net>
2026-05-10 01:33:52 -0500

    Update strainImages.js

--- data/strainImages.js
+++ data/strainImages.js
@@ -1,1 +1,13 @@
 
+const strainImages = {
+  "Cherry Bomb": "cherry-bomb.png",
+  "Blueberry Kush": "blueberry-kush.png",
+  "LSD": "lsd.png",
+  "Death Bubba": "death-bubba.png",
+  "Purple Rock Candy": "purple-rock-candy.png",
+  "Spiked Punch": "spiked-punch.png",
+  "Red Dragon": "red-dragon.png",
+  "Red Dawn": "red-dawn.png",
+  "Purple Dawn": "purple-dawn.png",
+  "Purple Punch": "purple-punch.png"
+};
